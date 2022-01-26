4075d729696015a16ca180f03b6e545dcf8d4b8d
alonhal <alonhal@il.ibm.com>
2022-01-26 18:41:45 +0200

    add ArgumentParser to start_sleuth.py

--- start_sleuth.py
+++ start_sleuth.py
@@ -1,5 +1,8 @@
+from argparse import ArgumentParser
 from lrtc_lib import app
-import sys
 
 if __name__ == '__main__':
-    app.start_server(sys.argv[1])
+    parser = ArgumentParser()
+    parser.add_argument('--port', type=int, help='Port number. Default is 8000',default=8000)
+    args = parser.parse_args()
+    app.start_server(args.port)
