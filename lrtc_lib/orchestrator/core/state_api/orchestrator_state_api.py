d1b53ed50432f7109b2d2d835611e7d6ada2b9fe
Ariel <ariel.gera1@ibm.com>
2022-05-08 17:44:31 +0300

    reorder the methods in orchestrator_state_api

--- lrtc_lib/orchestrator/core/state_api/orchestrator_state_api.py
+++ lrtc_lib/orchestrator/core/state_api/orchestrator_state_api.py
@@ -1,35 +1,30 @@
-from typing import Sequence, Mapping, Dict, List
-import jsonpickle
 import os
-from collections import OrderedDict, defaultdict
-from dataclasses import dataclass
-from dataclasses import field
-from datetime import datetime
-from enum import Enum
 import functools
 import threading
 
+from dataclasses import dataclass, field
+from datetime import datetime
+from enum import Enum
+from typing import Dict, List, Sequence
+
+import jsonpickle
 
 from lrtc_lib.definitions import WORKSPACE_DATA_DIR
 from lrtc_lib.models.core.model_api import ModelStatus
 from lrtc_lib.models.core.model_types import ModelTypes
 
-WORKSPACE_CLASS_VERSION = 2
+workspaces = dict()  # in-memory cache for Workspace objects
+workspaces_lock = threading.RLock()  # lock for all methods that access or manipulate the workspaces
 
 
 class IterationStatus(Enum):
     TRAINING = 0
-    RUNNING_INFERENCE = 1 # over the whole dataset
+    RUNNING_INFERENCE = 1
     RUNNING_ACTIVE_LEARNING = 2
     CALCULATING_STATISTICS = 3
     READY = 4
     ERROR = 5
     MODEL_DELETED = 6
-
-    # MODEL_NOT_READY = 0
-    # AL_NOT_STARTED = 1
-    # AL_IN_PROGRESS = 2
-    # READY = 3
 
 
 @dataclass
@@ -42,7 +37,7 @@ class ModelInfo:
 
 
 @dataclass
-class ActiveLearningIteration: # do we have a better name for this Iteration?
+class ActiveLearningIteration:  # TODO do we have a better name for this Iteration? change name throughout documentation
     model: ModelInfo
     status: IterationStatus
     iteration_statistics: Dict = field(default_factory=dict)
@@ -54,47 +49,26 @@ class Category:
     name: str
     description: str
     label_change_count_since_last_train: int = 0
-    active_learning_iterations: List[ActiveLearningIteration] = field(default_factory=list) # can be changed to SortedDict
+    active_learning_iterations: List[ActiveLearningIteration] = field(default_factory=list)
 
 
 @dataclass
 class Workspace:
     workspace_id: str
     dataset_name: str
-    categories: Dict[str, Category] = field(default_factory=dict) #category_to_description: 'dict' = field(default_factory=dict)
-    #category_to_number_of_label_changed: 'dict' = field(default_factory=dict)
-    # category_to_models: OrderedDict = field(default_factory=OrderedDict)  # category_name to model_id to ModelInfo
-    # category_to_model_to_recommendations: 'dict' = field(
-    #     default_factory=lambda: defaultdict(OrderedDict))  # category to {model_id:[TextElement...]}
-    # category_to_model_to_recommendations_status: 'dict' = field(default_factory=lambda: defaultdict(OrderedDict))
-    workspace_class_version: 'int' = WORKSPACE_CLASS_VERSION
-
-    @classmethod
-    def get_field_names(cls):
-        return cls.__annotations__.keys()
+    categories: Dict[str, Category] = field(default_factory=dict)
 
 
-
-
-
-
-
-
-lock = threading.RLock()
-
-
-def withlock(func): # TODO lock per workspace
+def withlock(func):  # TODO lock per workspace
     @functools.wraps(func)
-    def wrapper(*a, **k):
-        with lock:
-            return func(*a, **k)
+    def wrapper(*args, **kwargs):
+        with workspaces_lock:
+            return func(*args, **kwargs)
 
     return wrapper
 
 
-def _filename_from_workspace_id(workspace_id: str):
-    return workspace_id + ".json"
-
+# Workspace
 
 @withlock
 def create_workspace(workspace_id: str, dataset_name: str):
@@ -105,14 +79,17 @@ def create_workspace(workspace_id: str, dataset_name: str):
 
     if _filename_from_workspace_id(workspace_id) in os.listdir(WORKSPACE_DATA_DIR):
         raise Exception(f"workspace name '{workspace_id}' already exists")
-
     _save_workspace(workspace)
 
 
-def _save_workspace(workspace: Workspace):
-    workspace_encoded = jsonpickle.encode(workspace)
-    with open(os.path.join(WORKSPACE_DATA_DIR, _filename_from_workspace_id(workspace.workspace_id)), 'w') as f:
-        f.write(workspace_encoded)
+@withlock
+def get_workspace(workspace_id) -> Workspace:
+    return _load_workspace(workspace_id)
+
+
+@withlock
+def workspace_exists(workspace_id: str) -> bool:
+    return os.path.exists(os.path.join(WORKSPACE_DATA_DIR, _filename_from_workspace_id(workspace_id)))
 
 
 @withlock
@@ -123,12 +100,15 @@ def delete_workspace_state(workspace_id: str):
 
 
 @withlock
-def delete_category_from_workspace(workspace_id: str, category_name: str):
-    workspace = _load_workspace(workspace_id)
-    workspace.categories.pop(category_name)
-
-
-workspaces = dict()
+def get_all_workspaces() -> Sequence[Workspace]:
+    all_workspaces = []
+    for file in os.listdir(WORKSPACE_DATA_DIR):
+        if file.startswith('.') or not file.endswith('.json'):
+            continue
+        workspace_id, _ = os.path.splitext(file)
+        workspace = _load_workspace(workspace_id)
+        all_workspaces.append(workspace)
+    return all_workspaces
 
 
 def _load_workspace(workspace_id) -> Workspace:
@@ -142,12 +122,31 @@ def _load_workspace(workspace_id) -> Workspace:
     return workspace
 
 
+def _save_workspace(workspace: Workspace):
+    workspace_encoded = jsonpickle.encode(workspace)
+    with open(os.path.join(WORKSPACE_DATA_DIR, _filename_from_workspace_id(workspace.workspace_id)), 'w') as f:
+        f.write(workspace_encoded)
+
+
+def _filename_from_workspace_id(workspace_id: str):
+    return workspace_id + ".json"
+
+
+# Category
+
 @withlock
 def add_category_to_workspace(workspace_id: str, category_name: str, category_description: str):
     workspace = _load_workspace(workspace_id)
     if category_name in workspace.categories:
         raise Exception(f"Category '{category_name}' already exists in workspace '{workspace_id}'")
     workspace.categories[category_name] = Category(name=category_name, description=category_description)
+    _save_workspace(workspace)
+
+
+@withlock
+def delete_category_from_workspace(workspace_id: str, category_name: str):
+    workspace = _load_workspace(workspace_id)
+    workspace.categories.pop(category_name)
     _save_workspace(workspace)
 
 
@@ -161,13 +160,20 @@ def get_current_category_recommendations(workspace_id: str, category_name: str) 
             return iteration.active_learning_recommendations
     return []
 
+
 @withlock
 def update_category_recommendations(workspace_id: str, category_name: str, iteration_index: int,
                                     recommended_items: Sequence[str]):
     workspace = _load_workspace(workspace_id)
-    workspace.categories[category_name].active_learning_iterations[iteration_index].active_learning_recommendations = \
-        recommended_items
+    workspace.categories[category_name].active_learning_iterations[iteration_index].active_learning_recommendations \
+        = recommended_items
     _save_workspace(workspace)
+
+
+@withlock
+def get_label_change_count_since_last_train(workspace_id: str, category_name: str) -> int:
+    workspace = _load_workspace(workspace_id)
+    return workspace.categories[category_name].label_change_count_since_last_train
 
 
 @withlock
@@ -185,16 +191,7 @@ def increase_label_change_count_since_last_train(workspace_id: str, category_nam
     _save_workspace(workspace)
 
 
-@withlock
-def get_label_change_count_since_last_train(workspace_id: str, category_name: str):
-    workspace = _load_workspace(workspace_id)
-    return workspace.categories[category_name].label_change_count_since_last_train
-
-
-@withlock
-def get_workspace(workspace_id):
-    return _load_workspace(workspace_id)
-
+# Iteration
 
 @withlock
 def add_iteration(workspace_id: str, category_name: str, model_info: ModelInfo):
@@ -204,39 +201,10 @@ def add_iteration(workspace_id: str, category_name: str, model_info: ModelInfo):
     _save_workspace(workspace)
 
 
-#
-# @withlock
-# def get_model_status(workspace_id: str, category_name: str, model_id):
-#     workspace = _load_workspace(workspace_id)
-#     models = workspace.category_to_models[category_name]
-#     return models[model_id].model_status
-#
-#
-
 @withlock
-def get_all_workspaces() -> Sequence[Workspace]:
-    all_workspaces = []
-    for file in os.listdir(WORKSPACE_DATA_DIR):
-        if file.startswith('.') or not file.endswith('.json'):
-            continue
-        workspace_id, _ = os.path.splitext(file)
-        workspace = _load_workspace(workspace_id)
-        all_workspaces.append(workspace)
-    return all_workspaces
-
-
-@withlock
-def update_model_state(workspace_id: str, category_name: str, iteration_index: int, new_status: ModelStatus):
+def get_iteration_status(workspace_id, category_name, iteration_index) -> IterationStatus:
     workspace = _load_workspace(workspace_id)
-    iterations = workspace.categories[category_name].active_learning_iterations
-    assert len(iterations) > iteration_index, f"Iteration '{iteration_index}' doesn't exist in workspace '{workspace_id}'"
-    iterations[iteration_index].model.model_status = new_status
-    _save_workspace(workspace)
-
-
-@withlock
-def workspace_exists(workspace_id: str) -> bool:
-    return os.path.exists(os.path.join(WORKSPACE_DATA_DIR, _filename_from_workspace_id(workspace_id)))
+    return workspace.categories[category_name].active_learning_iterations[iteration_index].status
 
 
 @withlock
@@ -247,10 +215,16 @@ def update_iteration_status(workspace_id, category_name, iteration_index, new_st
 
 
 @withlock
-def get_iteration_status(workspace_id, category_name, iteration_index):
+def get_all_iterations(workspace_id, category_name) -> List[ActiveLearningIteration]:
     workspace = _load_workspace(workspace_id)
-    return workspace.categories[category_name].active_learning_iterations[iteration_index].status
+    return workspace.categories[category_name].active_learning_iterations
 
+
+@withlock
+def get_all_iterations_by_status(workspace_id, category_name, status: IterationStatus) -> List[ActiveLearningIteration]:
+    workspace = _load_workspace(workspace_id)
+    return [iteration for iteration in workspace.categories[category_name].active_learning_iterations
+            if iteration.status == status]
 
 
 @withlock
@@ -262,22 +236,19 @@ def add_iteration_statistics(workspace_id, category_name, iteration_index, stati
 
 
 @withlock
+def update_model_state(workspace_id: str, category_name: str, iteration_index: int, new_status: ModelStatus):
+    workspace = _load_workspace(workspace_id)
+    iterations = workspace.categories[category_name].active_learning_iterations
+    assert len(iterations) > iteration_index,\
+        f"Iteration '{iteration_index}' doesn't exist in workspace '{workspace_id}'"
+    iterations[iteration_index].model.model_status = new_status
+    _save_workspace(workspace)
+
+
+@withlock
 def mark_iteration_model_as_deleted(workspace_id, category_name, iteration_index):
     workspace = _load_workspace(workspace_id)
     iteration = workspace.categories[category_name].active_learning_iterations[iteration_index]
     iteration.model.model_status = ModelStatus.DELETED
     iteration.status = IterationStatus.MODEL_DELETED
     _save_workspace(workspace)
-
-
-@withlock
-def get_all_iterations(workspace_id, category_name):
-    workspace = _load_workspace(workspace_id)
-    return workspace.categories[category_name].active_learning_iterations
-
-
-@withlock
-def get_all_iterations_by_status(workspace_id, category_name, status: IterationStatus):
-    workspace = _load_workspace(workspace_id)
-    return [iteration for iteration in workspace.categories[category_name].active_learning_iterations
-            if iteration.status == status]
