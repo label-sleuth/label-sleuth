5612f81cf9c4fada18f196675a46ae86490c5b63
alonhal <alonhal@il.ibm.com>
2022-05-09 15:59:28 +0300

    use dummy path for the disk cache unit test

--- lrtc_lib/models/util/test_disk_cache.py
+++ lrtc_lib/models/util/test_disk_cache.py
@@ -1,10 +1,11 @@
 import unittest
-
+import tempfile
+import os
 from lrtc_lib.models.core.model_api import Prediction
 from lrtc_lib.models.util.disk_cache import save_model_prediction_store_to_disk, load_model_prediction_store_from_disk
 
 
-class TestActiveLearningStrategies(unittest.TestCase):
+class TestDiskCache(unittest.TestCase):
     def test_save_equals_load(self):
         #[frozenset("text", "I love dogs"])
         cache = {('SVM_mid', (('text', 'Embrace growth and innovation!'),))
@@ -12,8 +13,9 @@ class TestActiveLearningStrategies(unittest.TestCase):
                  ('SVM_mid', (('text', 'parking lot'),))
                  : Prediction(False, 0.4)
                  }
-        save_model_prediction_store_to_disk("/tmp/dummy.json", cache)
-        loaded = load_model_prediction_store_from_disk("/tmp/dummy.json", Prediction)
+        cache_file_path = os.path.join(tempfile.TemporaryDirectory().name,"cache.json")
+        save_model_prediction_store_to_disk(cache_file_path, cache)
+        loaded = load_model_prediction_store_from_disk(cache_file_path, Prediction)
 
 
 
