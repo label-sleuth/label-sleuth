f457915f4cc6b10154f3fb632bcd12c0e0426113
Lucy Yip <Lucy.Yip@ibm.com>
2022-05-08 23:44:27 -0400

    dakuo's ppt

--- react_frontend/src/modules/Workplace/Presentation/index.jsx
+++ react_frontend/src/modules/Workplace/Presentation/index.jsx
@@ -12,9 +12,16 @@ export default function Presentation() {
     <div id="presentation">
       <div>
         <div class="exit_modal" onClick={close_introSlides}><img src={close_icon} alt="close info"/></div>
-        <label>Presentation Label</label>
-        <h2>Presentation Title</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label>System for Learning to Understand Text with Human-in-the-loop</label>
+        <h2>SLEUTH</h2>
+        <p>Data is the new oil of the digital era - when it is processed, analyzed and utilized efficiently and instantly, it will have a much greater value. We have a number of pain points for the current process:</p>
+        <ul>
+          <li>Limited technical expertise</li>
+          <li>Human time and effort required</li>
+          <li>Lack of transparency</li>
+        </ul>
+        <h4>Our Approach</h4>
+        <p>Sleuth aims to simplify and accelerate the generation of language models by bringing the human into the loop. Our solution will leverage, combine and further enhance the SOTA of various NLP models, and human-in-the-loop approaches, with the goal of creating a unified system for developing NLP algorithms with SMEs (Subject Matter Experts).</p>
       </div>
     </div>
   )
