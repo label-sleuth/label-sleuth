b1dcb5fdc2876a1391ead85271c97b3d5713044b
Ariel Gera <ariel.gera1@ibm.com>
2022-06-12 12:50:06 +0300

    continue refactor to use flask application factory

--- label_sleuth/start_label_sleuth.py
+++ label_sleuth/start_label_sleuth.py
@@ -1,17 +1,16 @@
 import os
 from argparse import ArgumentParser
 
+from label_sleuth import app
 from label_sleuth.config import load_config
 
-os.environ['SLEUTH_ROOT'] = os.path.abspath(os.path.join(__file__, os.pardir, ''))
+PROJECT_ROOT = os.path.abspath(os.path.join(__file__, os.pardir))
 
 
 if __name__ == '__main__':
-    from label_sleuth import app
     parser = ArgumentParser()
     parser.add_argument('--port', type=int, help='Port number. Default is 8000', default=8000)
     args = parser.parse_args()
-    root_dir = os.environ.get('SLEUTH_ROOT')
-    app.create_app(load_config(os.path.join(root_dir, "config.json")),
-                  root_dir)
-    app.start_server(args.port)
+    curr_app = app.create_app(config=load_config(os.path.join(PROJECT_ROOT, "config.json")),
+                              output_dir=os.path.join(PROJECT_ROOT, 'output'))
+    app.start_server(curr_app, args.port)
