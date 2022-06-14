df6cd9c21229b82ad99cb095f2c53da4f73c9b20
Martin Santillan Cooper <msantillancooper@ibm.com>
2022-06-14 16:07:28 -0300

    feat: improve tooltip messages

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,7 +1,7 @@
-export const SEARCH_ALL_DOCS_TOOLTIP_MSG = 'Search all documents (supports regular expressions)'
-export const NEXT_TO_LABEL_TOOLTIP_MSG = 'Next to label suggest to label these texts to best assist its learning process'
-export const PREV_DOC_TOOLTIP_MSG = "Go to the previous document"
-export const NEXT_DOC_TOOLTIP_MSG = "Go to the next document"
+export const SEARCH_ALL_DOCS_TOOLTIP_MSG = 'Search all documents'
+export const NEXT_TO_LABEL_TOOLTIP_MSG = 'Recommended to label' 
+export const PREV_DOC_TOOLTIP_MSG = "Go to previous document"
+export const NEXT_DOC_TOOLTIP_MSG = "Go to next document"
 export const SEARCH = 'search'
 export const RCMD = 'rcmd'
 export const RIGHT_DRAWER_WIDTH = 360
