b2585019799f120395175c44910e1cfa24fb8406
Ariel Gera <ariel.gera1@ibm.com>
2022-06-16 11:08:57 +0300

    change default output path to home directory

--- label_sleuth/start_label_sleuth.py
+++ label_sleuth/start_label_sleuth.py
@@ -6,16 +6,16 @@ import logging
 from label_sleuth import app
 from label_sleuth.config import load_config
 
+
 PROJECT_ROOT = os.path.abspath(os.path.join(__file__, os.pardir))
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--port', type=int, help='Port number. Default is 8000', default=8000)
 
-
-    default_output = os.path.join(os.getcwd(), 'output')
-    parser.add_argument('--output_path', type=str, help=f'output directory. Default is {default_output}',
-                        default=default_output)
+    default_output = os.path.join(str(Path.home()), 'label-sleuth')
+    parser.add_argument('--output_path', type=str, help=f'output directory. Default is your home directory: '
+                                                        f'{default_output}', default=default_output)
     config_path = os.path.join(PROJECT_ROOT, "config.json")
     parser.add_argument('--config_path', type=str, help=f'Config file path. Default is {config_path}',
                         default=config_path)
