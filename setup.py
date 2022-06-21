8d400caa25ce79720bc6115febf90c1e4664b0f9
Ilya Shnayderman <ilyashn@il.ibm.com>
2022-06-21 16:06:16 +0300

    take version from the tag

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="label-sleuth",
-    version="0.2.4",
+    version="RELEASE_VERSION".replace('v', ''),
     author="IBM Research",
     author_email="eyals@il.ibm.com",
     url="https://github.com/label-sleuth/label-sleuth",
