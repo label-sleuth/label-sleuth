e25cbdc0474e4a3946071642191fde498aa07b9b
ALON HALFON <ALONHAL@il.ibm.com>
2022-06-22 11:18:05 +0300

    add a parameter for hostname or address and use localhost by default

--- label_sleuth/start_label_sleuth.py
+++ label_sleuth/start_label_sleuth.py
@@ -21,11 +21,14 @@ import logging
 from label_sleuth import app
 from label_sleuth.config import load_config
 
-
 PROJECT_ROOT = os.path.abspath(os.path.join(__file__, os.pardir))
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+
+    parser.add_argument('--host', type=str,
+                        help='Hostname or IP address (string) on which to listen. Default is localhost',
+                        default="localhost")
     parser.add_argument('--port', type=int, help='Port number. Default is 8000', default=8000)
 
     default_output = os.path.join(str(Path.home()), 'label-sleuth')
@@ -43,4 +46,4 @@ if __name__ == '__main__':
     curr_app = app.create_app(config=load_config(args.config_path),
                               output_dir=args.output_path)
     logging.info(f"starting label-sleuth using config file {args.config_path}. Output directory is {args.output_path}")
-    app.start_server(curr_app, args.port, args.num_serving_threads)
+    app.start_server(curr_app, args.host, args.port, args.num_serving_threads)
