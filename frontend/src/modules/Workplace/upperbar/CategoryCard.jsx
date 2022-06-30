b6bb970bf50b69f9649e7028f50f9b26f812c87c
dina-ibm <102616716+dina-ibm@users.noreply.github.com>
2022-06-30 09:34:47 +0300

    fix: resizing the category card

--- frontend/src/modules/Workplace/upperbar/CategoryCard.jsx
+++ frontend/src/modules/Workplace/upperbar/CategoryCard.jsx
@@ -3,15 +3,18 @@ import CloseIcon from '@mui/icons-material/Close';
 import classes from "./UpperBar.module.css";
 
 export const CategoryCard = ({ setCardOpen }) => (
-  
+  <>
+   <div className={classes["arrow-left"]}></div>
     <Card className={classes["category-card"]}>
-      <CardContent>
-        <Typography variant="caption" color="text.secondary">
+      <CardContent sx={{padding: '0px 10px 10px 10px'}}>
+        <Typography sx={{lineHeight: '1.1'}} variant="caption" color="text.secondary">
             Please select a category or create a new one
         </Typography>
         <IconButton aria-label="close" style={{fontSize: "1rem"}} onClick={() => setCardOpen(false)}>
             <CloseIcon fontSize="inherit" />
           </IconButton>
       </CardContent>
-    </Card>
+    </Card>  
+  </>
+
 )
