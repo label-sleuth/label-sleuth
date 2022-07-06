e15229a90a1e77a76b262ed22f6a47127b0afb76
Martin Santillan Cooper <msantillancooper@ibm.com>
2022-07-06 14:41:27 -0300

    docs: add copyright comment to test files

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -13,10 +13,6 @@
     limitations under the License.
 */
 
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
