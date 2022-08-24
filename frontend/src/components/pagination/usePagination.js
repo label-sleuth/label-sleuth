4968ce8d66817c9ce08110814659e9f4bc2eda6c
Martin Santillan Cooper <msantillancooper@ibm.com>
2022-08-24 12:58:35 -0300

    refactor!: major code quality and performance improvement

--- frontend/src/components/pagination/usePagination.js
+++ frontend/src/components/pagination/usePagination.js
@@ -13,7 +13,6 @@
     limitations under the License.
 */
 
-import React from 'react';
 import { useMemo } from 'react';
 
 export const DOTS = '...';
--- frontend/src/modules/Workplace/redux/categorySlice.js
+++ frontend/src/modules/Workplace/redux/categorySlice.js
@@ -15,152 +15,164 @@
 
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { BASE_URL, WORKSPACE_API } from "../../../config";
-import { sidebarOptionEnum } from "../../../const";
-import { initialState } from './DataSlice'
+import { panelIds } from "../../../const";
+import { handleError } from "../../../utils/utils";
+import { initialState as panelsInitialState } from "./panelsSlice";
+
 const getWorkspace_url = `${BASE_URL}/${WORKSPACE_API}`;
 
+export const createCategoryOnServer = createAsyncThunk(
+  "workspace/createCategoryOnServer",
+  async (request, { getState }) => {
+    const state = getState();
 
-export const createCategoryOnServer = createAsyncThunk('workspace/createCategoryOnServer', async (request, { getState }) => {
+    const { category } = request;
 
-    const state = getState()
-
-    const { category } = request
-
-    var url = `${getWorkspace_url}/${encodeURIComponent(state.workspace.workspaceId)}/category`
-
-    const data = await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${state.authenticate.token}`
-        },
-        body: JSON.stringify({
-            'category_name': category,
-            'category_description': "",
-            'update_counter': true
-        }),
-        method: "POST"
-    }).then(response => response.json())
-
-    return data
-})
-
-export const deleteCategory = createAsyncThunk('workspace/deleteCategory', async (request, { getState }) => {
-
-    const state = getState()
-
-    var url = `${getWorkspace_url}/${encodeURIComponent(state.workspace.workspaceId)}/category/${state.workspace.curCategory}`
+    var url = `${getWorkspace_url}/${encodeURIComponent(
+      state.workspace.workspaceId
+    )}/category`;
 
     const data = await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${state.authenticate.token}`
-        },
-        method: "DELETE"
-    }).then(response => response.json())
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${state.authenticate.token}`,
+      },
+      body: JSON.stringify({
+        category_name: category,
+        category_description: "",
+        update_counter: true,
+      }),
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .catch((err) => console.log(err));
 
-    return data
-})
+    return data;
+  }
+);
 
-export const editCategory = createAsyncThunk('workspace/editCategory', async ({newCategoryName, newCategoryDescription}, { getState }) => {
+export const deleteCategory = createAsyncThunk(
+  "workspace/deleteCategory",
+  async (request, { getState }) => {
+    const state = getState();
 
-    const state = getState()
+    var url = `${getWorkspace_url}/${encodeURIComponent(
+      state.workspace.workspaceId
+    )}/category/${state.workspace.curCategory}`;
 
-    var url = `${getWorkspace_url}/${encodeURIComponent(state.workspace.workspaceId)}/category/${state.workspace.curCategory}`
+    const data = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${state.authenticate.token}`,
+      },
+      method: "DELETE",
+    }).then((response) => response.json());
+
+    return data;
+  }
+);
+
+export const editCategory = createAsyncThunk(
+  "workspace/editCategory",
+  async ({ newCategoryName, newCategoryDescription }, { getState }) => {
+    const state = getState();
+
+    var url = `${getWorkspace_url}/${encodeURIComponent(
+      state.workspace.workspaceId
+    )}/category/${state.workspace.curCategory}`;
 
     const body = JSON.stringify({
-        category_name: newCategoryName,
-        category_description: newCategoryDescription
-    })
+      category_name: newCategoryName,
+      category_description: newCategoryDescription,
+    });
 
     const data = await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${state.authenticate.token}`
-        },
-        body: body,
-        method: "PUT"
-    }).then(response => response.json())
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${state.authenticate.token}`,
+      },
+      body: body,
+      method: "PUT",
+    }).then((response) => response.json());
 
-    return data
-})
+    return data;
+  }
+);
 
-export const fetchCategories = createAsyncThunk('workspace/get_all_categories', async (request, { getState }) => {
+export const fetchCategories = createAsyncThunk(
+  "workspace/get_all_categories",
+  async (request, { getState }) => {
+    const state = getState();
 
-    const state = getState()
-
-    var url = `${getWorkspace_url}/${encodeURIComponent(state.workspace.workspaceId)}/categories`
+    var url = `${getWorkspace_url}/${encodeURIComponent(
+      state.workspace.workspaceId
+    )}/categories`;
 
     const data = await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${state.authenticate.token}`
-        },
-        method: "GET"
-    }).then(response => response.json())
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${state.authenticate.token}`,
+      },
+      method: "GET",
+    }).then((response) => response.json());
 
-    return data
-})
+    return data;
+  }
+);
 
 export const reducers = {
-    updateCurCategory(state, action) {
-        const c = action.payload
-        return {
-            ...state,
-            curCategory: c,
-            nextModelShouldBeTraining: false
-        }
-    },
-}
+  updateCurCategory(state, action) {
+    const c = action.payload;
+    return {
+      ...state,
+      curCategory: c,
+      nextModelShouldBeTraining: false,
+    };
+  },
+};
 
 export const extraReducers = {
-    [fetchCategories.fulfilled]: (state, action) => {
-        const data = action.payload
-
-        return {
-            ...state,
-            categories: data['categories']
-        }
-    },
-    [createCategoryOnServer.fulfilled]: (state, action) => {
-        // TODO: action.payload has an update_counter field that is not used, remove it
-        return {
-            ...state,
-            curCategory: action.payload.category_id.toString(),
-            categories: [...state.categories, action.payload],
-            nextModelShouldBeTraining: false
-        }
-    },
-    [deleteCategory.pending]: (state, action) => {
-        return {
-            ...state,
-            deletingCategory: true
-        }
-    },
-    [deleteCategory.fulfilled]: (state, action) => {
-        return {
-            ...initialState,
-            curDocName: state.curDocName,
-            documents: state.documents,
-            elements: state.elements,
-            deletingCategory: false,
-            categories: state.categories.filter(c => c.category_id != state.curCategory),
-            activePanel: sidebarOptionEnum.SEARCH,
-            workspaceId: state.workspaceId
-        }
-    },
-    [editCategory.fulfilled]: (state, action) => {
-        const { category_name, category_description } = action.payload;
-        return {
-          ...state,
-          categories: state.categories.map((c) =>
-            c.category_id == state.curCategory
-              ? {
-                  ...c,
-                  category_name,
-                  category_description,
-                }
-              : c
-          ),
-        };
-    },
-}
+  [fetchCategories.fulfilled]: (state, action) => {
+    state.categories = action.payload["categories"];
+  },
+  [createCategoryOnServer.fulfilled]: (state, action) => {
+    // TODO: action.payload has an update_counter field that is not used, remove it
+    return {
+      ...state,
+      curCategory: action.payload.category_id.toString(),
+      categories: [...state.categories, action.payload],
+      nextModelShouldBeTraining: false,
+    };
+  },
+  [createCategoryOnServer.rejected]: (state, action) => {
+    state.errorMessage = handleError(action.error);
+  },
+  [deleteCategory.pending]: (state, action) => {
+    state.deletingCategory = true;
+  },
+  [deleteCategory.fulfilled]: (state, action) => {
+    state.deletingCategory = false;
+    state.categories = state.categories.filter(
+      (c) => c.category_id != state.curCategory
+    );
+    state.curCategory = null;
+    state.modelVersion = null;
+    state.panels = {
+        ...panelsInitialState.panels,
+        activePanelId: panelIds.SEARCH,
+        [panelIds.MAIN_PANEL]: panelsInitialState.panels[panelIds.MAIN_PANEL],
+    }
+  },
+  [editCategory.fulfilled]: (state, action) => {
+    const { category_name, category_description } = action.payload;
+    state.categories = state.categories.map((c) =>
+      c.category_id == state.curCategory
+        ? {
+            ...c,
+            category_name,
+            category_description,
+          }
+        : c
+    );
+  },
+};
