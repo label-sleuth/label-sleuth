f7f51095736e040a07dfe6de4f1c30ffc3b89d7a
Martin Santillan Cooper <msantillancooper@ibm.com>
2022-09-26 15:45:18 -0300

    feat(frontend): add support for downloading weak labels

--- frontend/src/modules/Workplace/information/FileTransferLabels/TransferLabelsDialog.jsx
+++ frontend/src/modules/Workplace/information/FileTransferLabels/TransferLabelsDialog.jsx
@@ -1,12 +1,23 @@
 import * as React from "react";
-import { Stack, List, ListItem } from "@mui/material";
 import {
   LargeTitle,
   MainContent,
   PrimaryButton,
   SecondaryButton,
 } from "../../../../components/dialog";
-import { Box, Modal, TextField } from "@mui/material";
+import {
+  Box,
+  Modal,
+  TextField,
+  FormControlLabel,
+  FormGroup,
+  Checkbox,
+  Collapse,
+  IconButton,
+  Stack,
+  Typography,
+  Button,
+} from "@mui/material";
 import "./styles.css";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -15,7 +26,10 @@ import {
   downloadModel,
 } from "../../redux/DataSlice";
 import { curCategoryNameSelector } from "../../redux/DataSlice";
-
+import { blue } from "@mui/material/colors";
+import { styled } from "@mui/material/styles";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 export const UploadLabelsDialog = ({ open, setOpen }) => {
   const dispatch = useDispatch();
 
@@ -87,20 +101,99 @@ export const UploadLabelsDialog = ({ open, setOpen }) => {
   );
 };
 
+const ExpandMore = (props) => {
+  const { expand, ...other } = props;
+  return (
+    <IconButton
+      sx={{
+        color: "white",
+        pl: 0
+      }}
+      {...other}
+    />
+  );
+}
+
+const WeakLabelsOption = ({ checked, handleChange }) => {
+  const checkFormDescription = `
+    Download the train set that Label Sleuth used to train its latest classifier. 
+    As opposed to the labeled data, the train set can include labeled elements that 
+    were not directly labeled by the user, but were added using some automatic methods 
+    to improve the model quality.`
+  const [expanded, setExpanded] = React.useState(false);
+  return (
+    <Box>
+      <Stack
+        direction="row"
+        sx={{
+          alignItems: "center",
+        }}
+      >
+        <ExpandMore
+          expand={expanded}
+          onClick={() => setExpanded((prev) => !prev)}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        </ExpandMore>
+        <Typography component={Button} onClick={() => setExpanded((prev) => !prev)} sx={{textTransform: "none", color: "white", pl:0}}> (advanced) Download label-sleuth train set </Typography>
+      </Stack>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <FormGroup sx={{ pl: 4 }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                sx={{
+                  color: blue[800],
+                  "&.Mui-checked": {
+                    color: blue[600],
+                  },
+                  pl: 0,
+                  //alignSelf: "start",
+                }}
+              />
+            }
+            sx={{ fontStyle: "italic" }}
+            label={checkFormDescription}
+          />
+        </FormGroup>
+      </Collapse>
+    </Box>
+  );
+};
+
 export const DownloadLabelsDialog = ({ open, setOpen }) => {
   const dispatch = useDispatch();
 
+  const [checked, setChecked] = React.useState(false);
+
   const handleClose = () => {
     setOpen(false);
+    // the checked state is not being reset when this modal is reopened 
+    // somehow it is not being unmounted, thats why I am setting check
+    // to false when closing it
+    setChecked(false);
   };
 
   const onClick = () => {
-    dispatch(downloadLabels());
-    setOpen(false);
+    dispatch(downloadLabels({ labeledOnly: !!!checked }));
+    handleClose();
+  };
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
   };
 
   return (
-    <Modal open={open} onClose={handleClose} disableRestoreFocus>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      disableRestoreFocus
+      keepMounted={false}
+    >
       <Box className="dialog-content">
         <div
           style={{
@@ -114,6 +207,7 @@ export const DownloadLabelsDialog = ({ open, setOpen }) => {
               Download all labels from the workspace as a csv file. Each row in
               the csv is a label for a specific element for a specific category.
             </p>
+            <WeakLabelsOption checked={checked} handleChange={handleChange} />
           </MainContent>
         </div>
         <Stack
