b907a1734d256f12e2571d29f79fe2c0d17e773b
Martin Santillan Cooper <msantillancooper@ibm.com>
2022-11-15 17:09:46 -0300

    fix: labeling a main element scrolls to the first element into view

--- frontend/src/modules/Workplace/sidebar/customHooks/useScrollElementIntoView.js
+++ frontend/src/modules/Workplace/sidebar/customHooks/useScrollElementIntoView.js
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getPanelDOMKey, scrollIntoElementView } from "../../../../utils/utils";
 import { useDispatch, useSelector } from "react-redux";
 import { panelIds } from "../../../../const";
+import { useWhyDidYouUpdate } from '../../../../customHooks/useWhyDidYouUpdate'
+import { usePrevious } from '../../../../customHooks/usePrevious'
 /**
  * This custom hook manage the behaviour of
  * scrolling into an element in the main panel
@@ -9,9 +11,36 @@ import { panelIds } from "../../../../const";
  */
 const useScrollMainPanelElementIntoView = () => {
   const dispatch = useDispatch();
-  const { elements } = useSelector((state) => state.workspace.panels[panelIds.MAIN_PANEL]);
-
   const { DOMKey: focusedElementDOMKey, hackyToggle } = useSelector((state) => state.workspace.panels.focusedElement);
+  
+  /**
+   * The following variables are used to only trigger the effect that
+   * is in charge of scrolling main elements into view when required, and that is
+   * when focused element changes or when the document changes.
+   * 
+   * elements is an object with key as elementid and the information of that element as its value
+   * elementKeys is an array with the keys of `elements`
+   * previousElementKeys is the value of the past `elementKeys` in case they changed
+   * elementKeysState changes only when a deep comparisson (arrayEquals) between `previousElementKeys` and `previousElementKeys` gives false
+   */
+
+  const { elements } = useSelector((state) => state.workspace.panels[panelIds.MAIN_PANEL]);
+  const elementKeys = useMemo(() => elements ? Object.keys(elements) : null)
+  const previousElementKeys = usePrevious(elementKeys)
+  const [elementKeysState, setElementKeysState] = useState(elementKeys)
+  
+  const arrayEquals = (a, b) => {
+    return Array.isArray(a) &&
+      Array.isArray(b) &&
+      a.length === b.length &&
+      a.every((val, index) => val === b[index]);
+  }
+
+  useEffect(() => {
+    if (!arrayEquals(previousElementKeys, elementKeys)) {
+      setElementKeysState(elementKeys)
+    }
+  }, [elementKeys])
 
   useEffect(() => {
     let element;
@@ -25,7 +54,7 @@ const useScrollMainPanelElementIntoView = () => {
       element = document.getElementById(firstElementDOMKey);
       scrollIntoElementView(element, false);
     }
-  }, [focusedElementDOMKey, hackyToggle, elements, dispatch]);
+  }, [focusedElementDOMKey, hackyToggle, elementKeysState, dispatch]);
 };
 
 export default useScrollMainPanelElementIntoView;
