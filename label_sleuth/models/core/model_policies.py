ee8553ad40aab990515ab14bf7166b8d1f86d024
Ariel Gera <ariel.gera1@ibm.com>
2023-02-19 17:03:02 +0200

    add policies for NB models

--- label_sleuth/models/core/model_policies.py
+++ label_sleuth/models/core/model_policies.py
@@ -24,6 +24,8 @@ class ModelPolicies:
     """
     STATIC_SVM_ENSEMBLE = StaticModelPolicy(ModelsCatalog.SVM_ENSEMBLE)
     STATIC_SVM_WORD_EMBEDDINGS = StaticModelPolicy(ModelsCatalog.SVM_OVER_WORD_EMBEDDINGS)
-    STATIC_SVM_GLOVE = StaticModelPolicy(ModelsCatalog.SVM_OVER_WORD_EMBEDDINGS) # for backward compatibility
+    STATIC_SVM_GLOVE = StaticModelPolicy(ModelsCatalog.SVM_OVER_WORD_EMBEDDINGS)  # for backward compatibility
     STATIC_SVM_BOW = StaticModelPolicy(ModelsCatalog.SVM_OVER_BOW)
+    STATIC_NB_WORD_EMBEDDINGS = StaticModelPolicy(ModelsCatalog.NB_OVER_WORD_EMBEDDINGS)
+    STATIC_NB_BOW = StaticModelPolicy(ModelsCatalog.NB_OVER_BOW)
     STATIC_HF_BERT = StaticModelPolicy(ModelsCatalog.HF_BERT)
