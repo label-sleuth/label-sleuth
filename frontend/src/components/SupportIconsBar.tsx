3322dbd291947eb7c2059a64f30fd449b98ea6cb
isidro <isidromolina@pursuit.org>
2023-03-07 17:00:18 -0500

    change file to .tsx, initialize component props

--- frontend/src/components/SupportIconsBar.tsx
+++ frontend/src/components/SupportIconsBar.tsx
@@ -13,52 +13,60 @@
     limitations under the License.
 */
 
-import { Link } from "@mui/material";
-import { Stack } from "@mui/material";
+import { Link, Stack, Tooltip } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSlack, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
-import { Tooltip } from "@mui/material";
+import { SxProps } from "@mui/system";
 import { SizeProp } from "@fortawesome/fontawesome-svg-core";
 
 interface SupportIconsBarProps {
-  sx: { [key: string]: string | number};
-  iconsSize?: SizeProp;
+	sx?: SxProps;
+	iconsSize?: SizeProp;
 }
 
-export const SupportIconsBar = ({ sx, iconsSize = "xl" }: SupportIconsBarProps) => {
-  return (
-    <Stack direction="row" alignItems="center" justifyContent="center" spacing={2} sx={{ ...sx }}>
-      <Tooltip title={"Join Slack"} placement="top">
-        <Link
-          href="https://join.slack.com/t/labelsleuth/shared_invite/zt-1j5tpz1jl-W~UaNEKmK0RtzK~lI3Wkxg"
-          target="_blank"
-          rel="noopener noreferrer"
-          color="inherit"
-        >
-          <FontAwesomeIcon size={iconsSize} icon={faSlack} />
-        </Link>
-      </Tooltip>
-      <Tooltip title={"Github"} placement="top">
-        <Link
-          href="https://github.com/label-sleuth/label-sleuth"
-          target="_blank"
-          rel="noopener noreferrer"
-          color="inherit"
-        >
-          <FontAwesomeIcon size={iconsSize} icon={faGithub} />
-        </Link>
-      </Tooltip>
-      <Tooltip title={"Documentation"} placement="top">
-        <Link
-          href="https://www.label-sleuth.org/docs/index.html"
-          target="_blank"
-          rel="noopener noreferrer"
-          color="inherit"
-        >
-          <FontAwesomeIcon size={iconsSize} icon={faBook} />
-        </Link>
-      </Tooltip>
-    </Stack>
-  );
+export const SupportIconsBar = ({
+	sx,
+	iconsSize = "xl",
+}: SupportIconsBarProps) => {
+	return (
+		<Stack
+			direction="row"
+			alignItems="center"
+			justifyContent="center"
+			spacing={2}
+			sx={{ ...sx }}
+		>
+			<Tooltip title={"Join Slack"} placement="top">
+				<Link
+					href="https://join.slack.com/t/labelsleuth/shared_invite/zt-1j5tpz1jl-W~UaNEKmK0RtzK~lI3Wkxg"
+					target="_blank"
+					rel="noopener noreferrer"
+					color="inherit"
+				>
+					<FontAwesomeIcon size={iconsSize} icon={faSlack} />
+				</Link>
+			</Tooltip>
+			<Tooltip title={"Github"} placement="top">
+				<Link
+					href="https://github.com/label-sleuth/label-sleuth"
+					target="_blank"
+					rel="noopener noreferrer"
+					color="inherit"
+				>
+					<FontAwesomeIcon size={iconsSize} icon={faGithub} />
+				</Link>
+			</Tooltip>
+			<Tooltip title={"Documentation"} placement="top">
+				<Link
+					href="https://www.label-sleuth.org/docs/index.html"
+					target="_blank"
+					rel="noopener noreferrer"
+					color="inherit"
+				>
+					<FontAwesomeIcon size={iconsSize} icon={faBook} />
+				</Link>
+			</Tooltip>
+		</Stack>
+	);
 };
