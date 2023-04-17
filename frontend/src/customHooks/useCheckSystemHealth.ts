248d0a4bf6a1d6c3fd008f9c82289eb6f7fb4d41
Martín Santillán Cooper <msantillancooper@ibm.com>
2023-04-17 11:59:56 -0300

    feat: don't fetch system health if previous request didn't finish

--- frontend/src/customHooks/useCheckSystemHealth.ts
+++ frontend/src/customHooks/useCheckSystemHealth.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import { client } from "../api/client";
 import { BASE_URL } from "../config";
@@ -7,30 +7,39 @@ import { useNotification } from "../utils/notification";
 export const useCheckSystemHealth = () => {
   const [systemOk, setSystemOk] = useState(true);
   const { notify, closeNotification } = useNotification();
+  let requestIsLoading = useRef(false);
 
   useEffect(() => {
     const systemDownNotification = "system-down-notification";
 
-    const  fetchSystemHealth = async () => {
+    const fetchSystemHealth = async () => {
       const url = `${BASE_URL}/health-check`;
       let systemIsOk: boolean;
       try {
+        requestIsLoading.current = true;
         const response = await client.get(url);
+        requestIsLoading.current = false;
         systemIsOk = response.data.ok; // ok will be always true
         closeNotification(systemDownNotification);
       } catch (e) {
+        requestIsLoading.current = false;
         systemIsOk = false;
-        notify("The system is down. Please try to re-run or log in again (if applicable)", {
-          toastId: systemDownNotification,
-          type: toast.TYPE.INFO
-        }, true);
+        notify(
+          "The system is down. Please try to re-run or log in again (if applicable)",
+          {
+            toastId: systemDownNotification,
+            type: toast.TYPE.INFO,
+          },
+          true
+        );
       }
       setSystemOk(systemIsOk);
-    }
-    
-    fetchSystemHealth()
+    };
+
+    fetchSystemHealth();
+
     const interval = setInterval(() => {
-      fetchSystemHealth();
+      !requestIsLoading.current && fetchSystemHealth();
     }, 15000);
 
     return () => clearInterval(interval);
