e17af65ddb475d75b5a256cde7bfc26be8411260
Martín Santillán Cooper <msantillancooper@ibm.com>
2023-05-03 12:13:21 -0300

    feat: show message when no positivi predictions available for evaluation

--- frontend/src/modules/Workplace/redux/evaluationSlice.ts
+++ frontend/src/modules/Workplace/redux/evaluationSlice.ts
@@ -14,13 +14,23 @@
 */
 
 import { createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import { getCategoryQueryString, getQueryParamsString, parseElements } from "../../../utils/utils";
+import {
+  getCategoryQueryString,
+  getQueryParamsString,
+  parseElements,
+} from "../../../utils/utils";
 import { BASE_URL, WORKSPACE_API } from "../../../config";
 import { PanelIdsEnum } from "../../../const";
 import { client } from "../../../api/client";
 import { getWorkspaceId } from "../../../utils/utils";
 import { RootState } from "../../../store/configureStore";
-import { Element, ElementsDict, FetchPanelElementsParams, UnparsedElement, WorkspaceState } from "../../../global";
+import {
+  Element,
+  ElementsDict,
+  FetchPanelElementsParams,
+  UnparsedElement,
+  WorkspaceState,
+} from "../../../global";
 
 const getWorkspace_url = `${BASE_URL}/${WORKSPACE_API}`;
 
@@ -33,9 +43,13 @@ export const getEvaluationElements = createAsyncThunk<
 >("workspace/getEvaluationElements", async (request, { getState }) => {
   const state = getState();
 
-  const queryParams = getQueryParamsString([getCategoryQueryString(state.workspace.curCategory)]);
+  const queryParams = getQueryParamsString([
+    getCategoryQueryString(state.workspace.curCategory),
+  ]);
 
-  var url = `${getWorkspace_url}/${encodeURIComponent(getWorkspaceId())}/precision_evaluation_elements${queryParams}`;
+  var url = `${getWorkspace_url}/${encodeURIComponent(
+    getWorkspaceId()
+  )}/precision_evaluation_elements${queryParams}`;
 
   const response = await client.get(url);
   return response.data;
@@ -47,55 +61,72 @@ export const getEvaluationResults = createAsyncThunk<
   {
     state: RootState;
   }
->("workspace/getEvaluationResults", async (changed_elements_count, { getState }) => {
-  const state = getState();
+>(
+  "workspace/getEvaluationResults",
+  async (changed_elements_count, { getState }) => {
+    const state = getState();
 
-  const ids: string[] = Object.values(state.workspace.panels.panels[PanelIdsEnum.EVALUATION].elements as ElementsDict).map(
-    (e: Element) => e.id
-  );
+    const ids: string[] = Object.values(
+      state.workspace.panels.panels[PanelIdsEnum.EVALUATION]
+        .elements as ElementsDict
+    ).map((e: Element) => e.id);
 
-  // we know that modelVersion wont be null but typescript ask to check
-  if (state.workspace.modelVersion === null) return;
+    // we know that modelVersion wont be null but typescript ask to check
+    if (state.workspace.modelVersion === null) return;
 
-  const iteration = state.workspace.modelVersion - 1;
-  const queryParams = getQueryParamsString([getCategoryQueryString(state.workspace.curCategory)]);
+    const iteration = state.workspace.modelVersion - 1;
+    const queryParams = getQueryParamsString([
+      getCategoryQueryString(state.workspace.curCategory),
+    ]);
 
-  var url = `${getWorkspace_url}/${encodeURIComponent(getWorkspaceId())}/precision_evaluation_elements${queryParams}`;
+    var url = `${getWorkspace_url}/${encodeURIComponent(
+      getWorkspaceId()
+    )}/precision_evaluation_elements${queryParams}`;
 
-  const response = await client.post(url, {
-    ids,
-    iteration,
-    changed_elements_count,
-  });
+    const response = await client.post(url, {
+      ids,
+      iteration,
+      changed_elements_count,
+    });
 
-  return response.data;
-});
+    return response.data;
+  }
+);
 
 export const cancelEvaluation = createAsyncThunk<
   { canceled: string },
-  number,
+  { changedElementsCount: number; cancelByAPI?: boolean },
   {
     state: RootState;
   }
->("workspace/cancelEvaluation", async (changed_elements_count, { getState }) => {
-  const state = getState();
+>(
+  "workspace/cancelEvaluation",
+  async ({ changedElementsCount, cancelByAPI = true }, { getState }) => {
+    const state = getState();
+    if (cancelByAPI) {
+      const queryParams = getQueryParamsString([
+        getCategoryQueryString(state.workspace.curCategory),
+      ]);
 
-  const queryParams = getQueryParamsString([getCategoryQueryString(state.workspace.curCategory)]);
+      var url = `${getWorkspace_url}/${encodeURIComponent(
+        getWorkspaceId()
+      )}/cancel_precision_evaluation${queryParams}`;
 
-  var url = `${getWorkspace_url}/${encodeURIComponent(getWorkspaceId())}/cancel_precision_evaluation${queryParams}`;
-
-  const response = await client.post(url, {
-    changed_elements_count,
-  });
-  return response.data;
-});
+      const response = await client.post(url, {
+        changed_elements_count: changedElementsCount,
+      });
+      return response.data;
+    }
+    return { canceled: "Evaluation canceled" };
+  }
+);
 
 export const reducers = {
   cleanEvaluationState(state: WorkspaceState, action: PayloadAction<void>) {
     state.panels.panels[PanelIdsEnum.EVALUATION] = {
       ...state.panels.panels[PanelIdsEnum.EVALUATION],
       isInProgress: false,
-      elements: {},
+      elements: null,
       initialElements: {},
       lastScore: null,
       scoreModelVersion: null,
@@ -106,9 +137,12 @@ export const reducers = {
 export const extraReducers = [
   {
     action: getEvaluationElements.fulfilled,
-    reducer: (state: WorkspaceState, action: PayloadAction<{ elements: UnparsedElement[] }>) => {
+    reducer: (
+      state: WorkspaceState,
+      action: PayloadAction<{ elements: UnparsedElement[] }>
+    ) => {
       if (state.curCategory === null) return;
-      
+
       const { elements: unparsedElements } = action.payload;
 
       const { elements: initialElements } = parseElements(
@@ -118,7 +152,8 @@ export const extraReducers = [
 
       state.panels.panels[PanelIdsEnum.EVALUATION].isInProgress = true;
       state.panels.loading[PanelIdsEnum.EVALUATION] = false;
-      state.panels.panels[PanelIdsEnum.EVALUATION].initialElements = initialElements;
+      state.panels.panels[PanelIdsEnum.EVALUATION].initialElements =
+        initialElements;
       state.panels.panels[PanelIdsEnum.EVALUATION] = {
         ...state.panels.panels[PanelIdsEnum.EVALUATION],
         initialElements,
@@ -141,7 +176,10 @@ export const extraReducers = [
   },
   {
     action: getEvaluationResults.fulfilled,
-    reducer: (state: WorkspaceState, action: PayloadAction<{ score: number }>) => {
+    reducer: (
+      state: WorkspaceState,
+      action: PayloadAction<{ score: number }>
+    ) => {
       const { score } = action.payload;
       state.panels.loading[PanelIdsEnum.EVALUATION] = false;
       state.panels.panels[PanelIdsEnum.EVALUATION] = {
@@ -178,7 +216,7 @@ export const extraReducers = [
         ...state.panels.panels[PanelIdsEnum.EVALUATION],
         initialElements: {},
         isInProgress: false,
-        elements: {},
+        elements: null,
         hitCount: null,
       };
     },
--- frontend/src/modules/Workplace/sidebar/EvaluationPanel.tsx
+++ frontend/src/modules/Workplace/sidebar/EvaluationPanel.tsx
@@ -17,7 +17,7 @@ import React, { useCallback } from "react";
 import { Box, Button, Stack, Divider } from "@mui/material";
 import classes from "./index.module.css";
 import { useAppDispatch, useAppSelector } from "../../../customHooks/useRedux";
-import { SidebarElement as Element } from "../../../components/element/SidebarElement";
+import { SidebarElement } from "../../../components/element/SidebarElement";
 import { checkStatus, getEvaluationResults, cancelEvaluation } from "../redux";
 import { ButtonGroup } from "@mui/material";
 import {
@@ -28,37 +28,81 @@ import {
   PanelIdsEnum,
   LabelTypesEnum,
 } from "../../../const";
-import { Header, PanelTypography, Loading } from "./components/commonComponents";
+import {
+  Header,
+  PanelTypography,
+  Loading,
+} from "./components/commonComponents";
 import { useFetchPanelElements } from "../../../customHooks/useFetchPanelElements";
+import { Element } from "../../../global";
 
 const EvaluationPanel = () => {
   const dispatch = useAppDispatch();
 
-  const loading = useAppSelector((state) => state.workspace.panels.loading[PanelIdsEnum.EVALUATION]);
-  const { elements, initialElements, isInProgress, lastScore, scoreModelVersion } = useAppSelector(
+  const loading = useAppSelector(
+    (state) => state.workspace.panels.loading[PanelIdsEnum.EVALUATION]
+  );
+  const {
+    elements,
+    initialElements,
+    isInProgress,
+    lastScore,
+    scoreModelVersion,
+  } = useAppSelector(
     (state) => state.workspace.panels.panels[PanelIdsEnum.EVALUATION]
   );
-  const isLoading = useAppSelector((state) => state.workspace.panels.loading[PanelIdsEnum.EVALUATION]);
+  //const elements: ElementsDict | null = {};
+  //const initialElements: ElementsDict | null = {};
+
+  const isLoading = useAppSelector(
+    (state) => state.workspace.panels.loading[PanelIdsEnum.EVALUATION]
+  );
   const modelVersion = useAppSelector((state) => state.workspace.modelVersion);
-  const nextModelShouldBeTraining = useAppSelector((state) => state.workspace.nextModelShouldBeTraining);
+  const nextModelShouldBeTraining = useAppSelector(
+    (state) => state.workspace.nextModelShouldBeTraining
+  );
 
-  const currentContentData = React.useMemo(() => elements && Object.values(elements), [elements]);
+  const currentContentData = React.useMemo<Element[] | null>(
+    () => (elements !== null ? Object.values(elements) : null),
+    [elements]
+  );
 
-  const fetchEvaluationElements = useFetchPanelElements({ panelId: PanelIdsEnum.EVALUATION });
+  const fetchEvaluationElements = useFetchPanelElements({
+    panelId: PanelIdsEnum.EVALUATION,
+  });
 
   const evaluationElementsCount = React.useMemo(
     () =>
       currentContentData
         ? currentContentData
             .map((element) => element.userLabel !== LabelTypesEnum.NONE)
-            .reduce((partialSum: number, isLabeled: boolean) => partialSum + (isLabeled ? 1 : 0), 0)
+            .reduce(
+              (partialSum: number, isLabeled: boolean) =>
+                partialSum + (isLabeled ? 1 : 0),
+              0
+            )
         : 0,
     [currentContentData]
   );
 
+  const noPositivePredictions = React.useMemo(
+    () => currentContentData !== null && currentContentData.length === 0,
+    [currentContentData]
+  );
+
   const submitButtonDisabled = React.useMemo(() => {
-    return !isInProgress || (currentContentData !== null && evaluationElementsCount !== currentContentData.length);
-  }, [isInProgress, evaluationElementsCount, currentContentData]);
+    return (
+      !isInProgress ||
+      (currentContentData !== null &&
+        evaluationElementsCount !== currentContentData.length) ||
+      noPositivePredictions
+    );
+  }, [
+    isInProgress,
+    evaluationElementsCount,
+    currentContentData,
+    noPositivePredictions,
+  ]);
 
   const onStartEvaluation = () => {
     fetchEvaluationElements();
@@ -67,23 +111,35 @@ const EvaluationPanel = () => {
   const getChangedElementsCount = useCallback(
     () =>
       elements !== null
-        ? Object.keys(elements).filter((k) => elements[k].userLabel !== initialElements[k].userLabel).length
+        ? Object.keys(elements).filter(
+            (k) => elements[k].userLabel !== initialElements[k].userLabel
+          ).length
         : 0,
     [elements, initialElements]
   );
 
   const submitEvaluation = useCallback(() => {
     const changedElementsCount = getChangedElementsCount();
-    dispatch(getEvaluationResults(changedElementsCount)).then(() => dispatch(checkStatus()));
+    dispatch(getEvaluationResults(changedElementsCount)).then(() =>
+      dispatch(checkStatus())
+    );
   }, [dispatch, getChangedElementsCount]);
 
   const onCancelEvaluation = useCallback(() => {
     const changedElementsCount = getChangedElementsCount();
-    dispatch(cancelEvaluation(changedElementsCount)).then(() => dispatch(checkStatus()));
-  }, [dispatch, getChangedElementsCount]);
+    dispatch(
+      cancelEvaluation({
+        changedElementsCount,
+        cancelByAPI: !!!noPositivePredictions,
+      })
+    ).then(() => dispatch(checkStatus()));
+  }, [dispatch, getChangedElementsCount, noPositivePredictions]);
 
   const progressLabel = React.useMemo(() => {
-    return currentContentData && `(${evaluationElementsCount}/${currentContentData.length})`;
+    return (
+      currentContentData &&
+      `(${evaluationElementsCount}/${currentContentData.length})`
+    );
   }, [evaluationElementsCount, currentContentData]);
 
   return (
@@ -91,10 +147,16 @@ const EvaluationPanel = () => {
       <Header message={"Precision evaluation"} />
       <Box sx={{ display: "flex", justifyContent: "center", mt: 1, mb: 2 }}>
         <Stack>
-          {isInProgress && (
-            <PanelTypography sx={{ fontSize: "1rem", pt: 2 }}>{`Progress: ${progressLabel}`}</PanelTypography>
+          {isInProgress && !noPositivePredictions && (
+            <PanelTypography
+              sx={{ fontSize: "1rem", pt: 2 }}
+            >{`Progress: ${progressLabel}`}</PanelTypography>
           )}
-          <ButtonGroup sx={{ pt: 2 }} variant="text" aria-label="text button group">
+          <ButtonGroup
+            sx={{ pt: 2 }}
+            variant="text"
+            aria-label="text button group"
+          >
             <Button
               sx={{ textTransform: "none" }}
               onClick={onStartEvaluation}
@@ -102,40 +164,74 @@ const EvaluationPanel = () => {
             >
               Start
             </Button>
-            <Button sx={{ textTransform: "none" }} onClick={submitEvaluation} disabled={submitButtonDisabled}>
+            <Button
+              sx={{ textTransform: "none" }}
+              onClick={submitEvaluation}
+              disabled={submitButtonDisabled}
+            >
               Submit
             </Button>
-            <Button sx={{ textTransform: "none" }} onClick={onCancelEvaluation} disabled={!!!isInProgress}>
+            <Button
+              sx={{ textTransform: "none" }}
+              onClick={onCancelEvaluation}
+              disabled={!!!isInProgress}
+            >
               Cancel
             </Button>
           </ButtonGroup>
         </Stack>
       </Box>
       <Divider variant="middle" />
-      <Box sx={{ mt: 2 }}>
-        {isLoading ? null : isInProgress ? (
-          <PanelTypography>{EVALUATION_IN_PROGRESS_MSG} </PanelTypography>
-        ) : lastScore !== null && scoreModelVersion !== null && modelVersion !== null ? (
+      {currentContentData?.length === 0 ? (
+        <Box sx={{ mt: 1 }}>
           <PanelTypography sx={{ fontSize: "1rem" }}>
-            {PRECISION_RESULT_MSG(Math.round(lastScore * 100), modelVersion, scoreModelVersion)}
+            {
+              "Currently it's not possible to evaluate the precision of the model because there are no positive predictions."
+            }
           </PanelTypography>
-        ) : nextModelShouldBeTraining ? (
-          <PanelTypography>{WAIT_NEW_MODEL_MSG}</PanelTypography>
-        ) : (
-          <PanelTypography>{START_EVALUATION_MSG}</PanelTypography>
-        )}
-      </Box>
-      <Box sx={{ mt: isInProgress ? 16 : 12 }} className={classes["element-list"]}>
-        {isLoading ? (
-          <Loading />
-        ) : isInProgress && currentContentData !== null ? (
-          <Box>
-            {currentContentData.map((element, i) => (
-              <Element element={element} updateCounterOnLabeling={false} key={element.id} index={i} />
-            ))}
+        </Box>
+      ) : (
+        <>
+          <Box sx={{ mt: 2 }}>
+            {isLoading ? null : isInProgress ? (
+              <PanelTypography>{EVALUATION_IN_PROGRESS_MSG} </PanelTypography>
+            ) : lastScore !== null &&
+              scoreModelVersion !== null &&
+              modelVersion !== null ? (
+              <PanelTypography sx={{ fontSize: "0.9rem" }}>
+                {PRECISION_RESULT_MSG(
+                  Math.round(lastScore * 100),
+                  modelVersion,
+                  scoreModelVersion
+                )}
+              </PanelTypography>
+            ) : nextModelShouldBeTraining ? (
+              <PanelTypography>{WAIT_NEW_MODEL_MSG}</PanelTypography>
+            ) : (
+              <PanelTypography sx={{fontSize:"0.9rem"}}>{START_EVALUATION_MSG}</PanelTypography>
+            )}
           </Box>
-        ) : null}
-      </Box>
+          <Box
+            sx={{ mt: isInProgress ? 16 : 12 }}
+            className={classes["element-list"]}
+          >
+            {isLoading ? (
+              <Loading />
+            ) : isInProgress && currentContentData !== null ? (
+              <Box>
+                {currentContentData.map((element, i) => (
+                  <SidebarElement
+                    element={element}
+                    updateCounterOnLabeling={false}
+                    key={element.id}
+                    index={i}
+                  />
+                ))}
+              </Box>
+            ) : null}
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
