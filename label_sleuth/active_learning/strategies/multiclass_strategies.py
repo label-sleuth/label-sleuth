9cf452fbdfda6a5857cea66fcc1122ed1b290014
ALON HALFON <ALONHAL@il.ibm.com>
2023-08-18 11:35:02 +0300

    feat(backend): multiclass active learning strategies and multiclass training set selector

---
+++ label_sleuth/active_learning/strategies/multiclass_strategies.py
@@ -0,0 +1,34 @@
+#
+#  Copyright (c) 2022 IBM Corp.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+from typing import Sequence, Union
+
+from scipy.stats._entropy import entropy
+
+from label_sleuth.active_learning.core.active_learning_api import ActiveLearner
+from label_sleuth.data_access.core.data_structs import TextElement
+from label_sleuth.models.core.prediction import Prediction, MulticlassPrediction
+
+
+class EntropyMulticlassLearner(ActiveLearner):
+    """
+    This active learning module suggests "hard" examples, i.e. examples that the model is most uncertain about.
+    The highest active learning scores are given to examples which were given model scores closest to the
+    decision threshold of 0.5.
+    """
+    def get_per_element_score(self, candidate_text_elements: Sequence[TextElement],
+                              candidate_text_element_predictions: Union[Sequence[Prediction], Sequence[MulticlassPrediction]], workspace_id: str,
+                              dataset_name: str, category_id: int) -> Sequence[float]:
+        return [entropy(pred.scores) for pred in candidate_text_element_predictions]
---
+++ label_sleuth/training_set_selector/multiclass_train_set_selectors.py
@@ -0,0 +1,32 @@
+from collections import Counter
+from typing import Sequence
+import logging
+import sys
+
+from label_sleuth.data_access.core.data_structs import TextElement
+from label_sleuth.training_set_selector.train_set_selector_api import TrainSetSelectorAPI
+
+
+class TrainSetSelectorMulticlassAllLabeled(TrainSetSelectorAPI):
+
+    def get_data_and_counts_for_labeled(self, workspace_id, dataset_name, category_id, remove_duplicates=False):
+        if dataset_name is None:
+            return None, None
+        labeled_elements = \
+            self.data_access.get_labeled_text_elements(workspace_id=workspace_id, dataset_name=dataset_name,
+                                                       category_id=category_id, sample_size=sys.maxsize,
+                                                       remove_duplicates=remove_duplicates,
+                                                       label_types=self.label_types)["results"]
+        labels = [element.label.label for element in labeled_elements]
+        counts = Counter(labels)
+
+        return labeled_elements, counts
+
+    def get_train_set(self, workspace_id: str, train_dataset_name: str, category_id: int, category_name: str,
+                      category_description: str) -> Sequence[TextElement]:
+        train_data, train_counts = self.get_data_and_counts_for_labeled(workspace_id, train_dataset_name, category_id,
+                                                                        remove_duplicates=True)
+        # if self.should_verify_all_labels_in_train:
+        #     self.verify_all_labels_are_in_train(train_counts)
+        logging.info(f"using {len(train_data)} for train using dataset {train_dataset_name}")
+        return train_data
